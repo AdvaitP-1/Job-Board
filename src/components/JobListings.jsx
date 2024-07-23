c631dfe18e96fbb40e61432a4d4fce8e9c9b3cce
Advait Pandey <advait.pandey05@gmail.com>
2024-07-22 23:29:32 -0400

    a

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import JobListing from './JobListing';
+import Spinner from './Spinner';
 
 // eslint-disable-next-line react/prop-types
 const JobListings = ({ isHome = false }) => {
@@ -8,8 +9,9 @@ const JobListings = ({ isHome = false }) => {
 
   useEffect(() => {
     const fetchJobs = async () => {
+      const apiUrl = isHome ? '/api/jobs?_limit=3' : '/api/jobs';
       try {
-        const res = await fetch('http://loaclhost:5000/jobs');
+        const res = await fetch(apiUrl);
         const data = await res.json();
         setJobs(data);
       } catch (error) {
@@ -24,24 +26,23 @@ const JobListings = ({ isHome = false }) => {
   }, []);
 
   return (
-      <section className=" px-4 py-10">
-      <div className="container-xl lg:container m-auto">
-        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          { isHome ? 'Recent Jobs' : 'Browse Jobs' }
+    <section className='bg-blue-50 px-4 py-10'>
+      <div className='container-xl lg:container m-auto'>
+        <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>
+          {isHome ? 'Recent Jobs' : 'Browse Jobs'}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          { loading ? (<h2>Loading...</h2>) : (
-            <>
-              { jobs.map((job) => (
+
+        {loading ? (
+          <Spinner loading={loading} />
+        ) : (
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+            {jobs.map((job) => (
               <JobListing key={job.id} job={job} />
-              )) }
-            </>
-          )}
-           
-        </div>
+            ))}
+          </div>
+        )}
       </div>
     </section>
-  )
-}
-
-export default JobListings
+  );
+};
+export default JobListings;
